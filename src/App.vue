81d08bd1b42645fe00e18cb66f43626cf467f48b
Osasere Ikponmwosa <edenosas1@gmail.com>
2024-10-15 07:16:58 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,27 @@
   <h1>DC HEROES</h1>
   <ul>
     <li v-for="(hero, key) in dcHeroes" v-bind:key="key">
-      {{ hero }} {{ key }}
+      {{ key }} {{ hero.name }}
     </li>
   </ul>
+  <input v-model.lazy.trim="newHero" />
+  <form @submit.prevent="dcHeroes[2].name = newHero">
+    <button type="submit">Add Hero</button>
+  </form>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      dcHeroes: ['Superman', 'Batman', 'Wonderwoman', 'Supergirl'],
+      isActive: false,
+      newHero: 'Aquaman',
+      dcHeroes: [
+        { name: 'Superman' },
+        { name: 'Batman' },
+        { name: 'Wonderwoman' },
+        { name: 'Supergirl' },
+      ],
     }
   },
 }
